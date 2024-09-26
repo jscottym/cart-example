77af9ba500128b65fab73a4e94c7a39526a45942
megnan-nicole <megan.martineau.school@gmail.com>
2024-09-26 10:46:21 +0200

    misc

---
+++ pages/flex.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'blank',
+});
+
+import { ref } from 'vue';
+
+</script>
+
+<template>
+    <div class="header">
+
+    </div>
+    <div class="wrapper">
+      <div class="box box-1">Box 1</div>
+      <div class="box box-2">Box 2</div>
+      <div class="box box-3">Box 3</div>
+      <div class="box box-4">Box 4</div>
+
+    </div>
+  </template>
+  
+<style scoped>
+
+.header {
+    position: sticky 20px;
+    width: 100px;
+  height: 100px;
+  
+}
+
+.wrapper {
+  display: flex;
+  justify-content: flex-start ;  
+  align-items: flex-start;
+  padding: 20px;
+  gap: .5rem;
+  min-height: 25rem;
+  border: 2px solid red;
+
+}
+
+.box { 
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.box-1 {
+    background-color: blue;
+    color: white;
+    height: 15rem;
+}
+
+
+.box-2 {
+    background-color: pink;
+    height: 10rem;
+}
+
+
+.box-3 {
+    background-color: teal;
+}
+
+.numbered-box {
+  font-size: 24px;
+  color: #fff;
+  font-weight: bold;
+}
+
+</style>
--- pages/styling.vue
+++ pages/styling.vue
@@ -5,33 +5,74 @@ definePageMeta({
 </script>
 
 <template>
-    <div class="scroll-container">
-        <div class="header">Header</div>
-        
-        <div class="wrapper">
-            <div 
-                v-for="i in 10" :key="i" class="box" 
-                :class="`box-${i}`"
-            >
-                box-{{ i }}
-            </div>
-        </div>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+
+<div class="box box-1">
+      box1  
+      <div class="box box-2">box2</div>
+      <div class="box box-3">box3</div>
     </div>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+    
+    <p class=" test gray">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis earum corporis quod nostrum alias doloremque mollitia qui tenetur saepe dolorem? Alias officia dolor praesentium animi. Impedit aliquam libero molestias necessitatibus?</p>
+
+    
+
+    <div class="scroll-container">
+        <div style="" class="header red">
+            <span style="color: blue;">Word</span><span style="color: red;">&nbsp;Another</span>
+        </div>
+
+        <div class="wrapper">
+            <div class="box box-1">box-1</div>
+            <div class="box box-2">box-2</div>
+            <div class="box box-3">box-3</div>
+            <div class="box box-4">box-4</div>
+            <div class="box box-5">box-5</div>
+            <div class="box box-6">box-6</div>
+            <div class="box box-7">box-7</div>
+            <div class="box box-8">box-8</div>
+            <div class="box box-9">box-9</div>
+            <div class="box box-10">box-10</div>
+        </div>
+        
+    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ullam est? Consectetur eaque consequuntur quos quas similique tenetur qui necessitatibus esse repudiandae, omnis libero distinctio magni laboriosam vel recusandae quibusdam?</p>
 </template>
 
 <style scoped>
+
 .scroll-container {
     max-height: 100dvh; /* Full viewport height */
     display: flex;
     flex-direction: column;
 }
 
+.red {
+    background-color: red;
+}
+
 .header {
     background-color: bisque;
-    padding: 1rem;
-    position: sticky; /* Ensure header stays at the top while scrolling */
-    top: 0;
-    z-index: 10;
+    padding: 16px;
 }
 
 .wrapper {
@@ -39,33 +80,35 @@ definePageMeta({
     border: 1px solid aquamarine;
     margin: 1rem;
     padding: 1rem;
+    display: none;
 }
 
 .box {
     padding: 1rem;
     border: 3px solid black;
-    box-sizing: border-box;
     width: 50%;
     min-height: 80px;
-    margin: 1rem auto;
-    position: relative;
 }
 
 .box-1 {
     background-color: darkmagenta;
     width: 100%;
-    position: sticky;
     top: 0;
-    z-index: 1;
 }
 
 .box-2 {
     background-color: burlywood;
-    width: 80%;
+    width: 75px;
+    top: 0;
+    margin-right: 1rem;
+    position: fixed;
 }
 
 .box-3 {
     background-color: darkolivegreen;
-    width: 60%;
+    position: absolute;
+    width: 75px;
+    top: 0;
+    left: 91px;
 }
 </style>
